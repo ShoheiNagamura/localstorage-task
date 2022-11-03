189de429a0bcbc9a8892763e31759a39ddbeec62
shouhei <shohei.nagamura@gmail.com>
2022-11-03 22:43:07 +0900

    メモ表示機能追加

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
-
-// const hoge = [
-//     {
-//         "name": "nagamura",
-//         "age": 28,
-//     },
-//     {
-//         "name": "shouhei",
-//         "age": 27
-//     }
-// ]
-
-// hoge.push({ name: "techann", age: 24 });
-
-// console.log(hoge);
-
-
 const memo = [];
+
+const hoge = [];
+
 
 // saveをクリックするとデータ保存
 $("#save").on('click', function () {
-    // フォームから値を受け取り変数に格納
+    console.log(hoge);
+    // フォームから値を受け取りdataオブジェクトに格納
     const data = {
         "title": $("#title").val(),
         "text": $("#text").val()
     }
-    memo.push(data);
+    memo.push(data); //memo配列に格納
 
-    for (let i = 0; i < memo.length - 1; i++) {
-        $(".list").append(memo[i].title);
-    }
+    // for (let i = 0; i < memo.length; i++) {
+    hoge.push("<p>" + memo.at(-1).title + "</p>");
+    // hoge配列をリバース
+    hoge.reverse();
+    console.log(hoge); //cba
+    console.log(memo); //abc
+    // }
+    $(".y").html(hoge);
+    hoge.reverse(); //abc
+    console.log(hoge); //abc
+    console.log(memo); //abc
 
     //JSONデータに変換しローカルストレージに格納
     const jsonMemo = JSON.stringify(memo);
@@ -41,11 +36,24 @@ if (localStorage.getItem("memo")) {
     const jsonMemo = localStorage.getItem("memo");
     // //取得したJSONデータをオブジェクトに変換
     const data = JSON.parse(jsonMemo);
+    // console.log(data); abc
     for (let i = 0; i < data.length; i++) {
-        memo.push(data[i]);
+        memo.push(data[i]); //abc
     }
 
-    for (let i = 0; i < memo.length - 1; i++) {
-        $(".list").append(memo[i].title);
-    }
+    // console.log(memo);
+    memo.reverse(); //cba
+
+    for (let i = 0; i < memo.length; i++) {
+        hoge.push("<p>" + memo[i].title + "</p>");
+    }//cba
+
+
+
+    console.log(hoge);
+
+    $(".y").html(hoge); //cba
+
+    memo.reverse(); //abc
+    hoge.reverse(); //abc
 }
